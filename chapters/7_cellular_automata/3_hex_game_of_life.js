b22b04eef05b642a3d26558e18b4cf3ea7d3d759
Nathan Pham <nathanpham.me@gmail.com>
2022-10-02 13:28:02 -0700

    working hex game of life + references

--- chapters/7_cellular_automata/3_hex_game_of_life.js
+++ chapters/7_cellular_automata/3_hex_game_of_life.js
@@ -1,14 +1,16 @@
 import useP5 from "/lib/useP5.js";
 import p5 from "p5";
 
+// https://gorillasun.de/blog/a-guide-to-hexagonal-grids-in-p5js
+
 export const canvas = useP5((p) => {
     const Grid = GridFactory(p);
-    const size = 20;
+    const size = 40;
 
     let grid;
 
     const setup = () => {
-        p.createCanvas(500, 500);
+        p.createCanvas(innerWidth, innerHeight - 50);
         grid = new Grid(size);
     };
 
@@ -23,8 +25,8 @@ export const canvas = useP5((p) => {
 
 function GridFactory(p) {
     return class Grid {
-        constructor(radius) {
-            this.radius = radius;
+        constructor(size) {
+            this.radius = Math.min(p.width, p.height) / size / 2;
             this.state = this.generateState();
         }
 
@@ -32,18 +34,24 @@ function GridFactory(p) {
             return this.radius * 2;
         }
 
-        get size() {
-            return Math.floor(p.width / this.radius);
+        get width() {
+            return Math.ceil(p.width / this.diameter / 1.5);
+        }
+
+        get height() {
+            return Math.ceil((p.height / this.diameter) * 2.3);
         }
 
         generateState(useRandom = true) {
             // create a 2D array of 0s
-            const state = new Array(this.size)
+            const state = new Array(this.height)
                 .fill(0)
                 .map(() =>
-                    new Array(this.size)
+                    new Array(this.width)
                         .fill(0)
-                        .map(() => (useRandom ? (p.random() > 0.9 ? 1 : 0) : 0))
+                        .map(() =>
+                            useRandom ? Math.floor(Math.random() * 1.33) : 0
+                        )
                 );
 
             return state;
@@ -51,9 +59,77 @@ function GridFactory(p) {
 
         nextGeneration() {
             this.generation++;
+            const newState = this.generateState(false);
+
+            for (let y = 0; y < this.state.length; y++) {
+                for (let x = 0; x < this.state[y].length; x++) {
+                    const neighbors = this.getNeighbors(x, y);
+
+                    const get = () => this.state[y][x];
+                    const set = (n = 0) => (newState[y][x] = n);
+
+                    // https://davidsiaw.github.io/blog/2014/11/21/hexlife/
+                    // if a live cell is surrounded by 3 or 4 live cells, it stays alive
+                    if (get() === 1 && (neighbors == 3 || neighbors == 4)) {
+                        set(1);
+                    }
+
+                    // if a dead cell is surrounded by 2 live cells, the dead cell becomes alive
+                    else if (get() === 0 && neighbors == 2) {
+                        set(1);
+                    }
+
+                    // in all other cases, the cell dies
+                    else {
+                        set(0);
+                    }
+                }
+            }
+
+            this.state = newState;
         }
 
-        getNeighbors(x, y) {}
+        getNeighbors(x, y) {
+            let neighbors = 0;
+
+            // get the neighbors of a hexagonal grid
+            // https://www.redblobgames.com/grids/hexagons/#neighbors-axial
+            // davidsiaw.github.io/hexlife/
+
+            const coords =
+                y % 2
+                    ? [
+                          [x, y - 1],
+                          [x - 1, y],
+                          [x, y + 1],
+                          [x + 1, y - 1],
+                          [x + 1, y],
+                          [x + 1, y + 1],
+                      ]
+                    : [
+                          [x - 1, y - 1],
+                          [x - 1, y],
+                          [x - 1, y + 1],
+                          [x, y - 1],
+                          [x + 1, y],
+                          [x, y + 1],
+                      ];
+
+            for (const coord of coords) {
+                let [xIdx, yIdx] = coord;
+
+                yIdx =
+                    (yIdx < 0 ? yIdx + this.state.length : yIdx) %
+                    this.state.length;
+                xIdx =
+                    (xIdx < 0 ? xIdx + this.state[0].length : xIdx) %
+                    this.state[0].length;
+
+                neighbors += this.state[yIdx][xIdx];
+            }
+
+            return neighbors;
+        }
 
         update() {
             this.nextGeneration();
@@ -76,38 +152,21 @@ function GridFactory(p) {
         }
 
         draw() {
-            // count = 0
-            // for(y = 0; y < gridHeight; y+=hexagonSize/2.3){
-            //   for(x = 0; x < gridWidth; x+=hexagonSize*1.5){
-            //     drawHexagon(x+hexagonSize*(count%2==0)*0.75, y, hexagonSize/2)
-            //   }
-            //   count ++
-            // }
-
             p.stroke(0);
-            p.noFill();
             for (let y = 0; y < this.state.length; y++) {
                 for (let x = 0; x < this.state[y].length; x++) {
-                    const _y = (y * this.radius) / 2.3;
-                    const _x = x * this.radius * 1.5;
+                    const _y = (y * this.diameter) / 2.3; // magic numbers?
+                    const _x = x * this.diameter * 1.5;
+                    const cell = this.state[y][x];
 
                     p.push();
+                    p.fill(cell === 1 ? 0 : 255);
                     p.translate(
-                        _x + this.radius * (y % 2 === 0 ? 0 : 0.75),
+                        _x + this.diameter * (y % 2 === 0 ? 0 : 0.75),
                         _y
                     );
                     this.drawPolygon();
                     p.pop();
-                    // const cell = this.state[y][x];
-
-                    // p.push();
-
-                    // // p.fill(cell === 1 ? 0 : 255);
-                    // // p.noStroke();
-                    // p.noFill();
-                    // p.stroke(0);
-                    // this.drawPolygon(0, 0);
-                    // p.pop();
                 }
             }
         }
