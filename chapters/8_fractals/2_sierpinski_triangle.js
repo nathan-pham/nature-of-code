b7ebbd6e867b7de7287204c834686a3c4a767e44
Nathan Pham <nathanpham.me@gmail.com>
2022-10-02 15:24:41 -0700

    cool triangle

--- chapters/8_fractals/2_sierpinski_triangle.js
+++ chapters/8_fractals/2_sierpinski_triangle.js
@@ -6,7 +6,7 @@ export const canvas = useP5((p) => {
         p.noLoop();
 
         p.background(255);
-        drawTriangle(p, p.width / 2, p.height / 2, 300);
+        drawTriangle(p, p.width / 2, p.height / 2, 200);
     };
 
     return { setup };
@@ -16,10 +16,18 @@ function drawTriangle(p, x, y, radius) {
     p.stroke(0);
     p.noFill();
 
-    // if (diameter > 10) {
-    //     drawCircle(p, x + radius, y, radius);
-    //     drawCircle(p, x - radius, y, radius);
-    //     drawCircle(p, x, y + radius, radius);
-    //     drawCircle(p, x, y - radius, radius);
-    // }
+    //  draw sierpinksi triangle recursively
+    if (radius > 2) {
+        p.triangle(
+            x - radius,
+            y + radius,
+            x + radius,
+            y + radius,
+            x,
+            y - radius
+        );
+        drawTriangle(p, x - radius / 2, y + radius / 2, radius / 2); // draw left triangle
+        drawTriangle(p, x + radius / 2, y + radius / 2, radius / 2); // draw right triangle
+        drawTriangle(p, x, y - radius / 2, radius / 2); // draw top triangle
+    }
 }
